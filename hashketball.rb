df0fefb99226c6837536aa5ec5fadb1dabecb966
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-22 14:55:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,10 +4,23 @@ def game_hash
     :home => {
       :team_name => "",
       :colors => [],
-      :players => [{}]
+      :players => [
+        :player_name
+        :number
+        :shoe
+        :points
+        :rebounds
+        :assists
+        :steals
+        :blocks
+        :slam_dunks
+        ]
     },
     :away => {
-      
+      :team_name => "",
+      :colors => [],
+      :players => [
+        ]
     }
   }
 end
